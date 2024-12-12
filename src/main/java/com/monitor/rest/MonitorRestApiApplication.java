659093fa99010f99b021d5e625c5fe866ac49975
nico-ortiz <nicortiz1234@gmail.com>
2024-12-12 12:44:32 -0300

    initial commit

---
+++ src/main/java/com/monitor/rest/MonitorRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.monitor.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monitor/rest/MonitorRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitor.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
